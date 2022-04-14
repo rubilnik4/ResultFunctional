e140f64e570a93883616b4c78d087fa9152f6d4d
Ivan Rubilnik <rubilnik4@yandex.ru>
2022-04-14 18:22:29 +0500

    Translate comments
    Translate comments

--- ResultFunctional/FunctionalExtensions/Sync/MapExtensions.cs
+++ ResultFunctional/FunctionalExtensions/Sync/MapExtensions.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ResultFunctional.FunctionalExtensions.Sync
 {
     /// <summary>
-    /// Методы расширения для преобразования типов
+    /// Functor functions
     /// </summary>
     public static class MapExtensions
     {
         /// <summary>
-        /// Преобразование типов с помощью функции
-        /// </summary>       
+        /// Converting source type to result type by functor function
+        /// </summary>
+        /// <typeparam name="TSource">Source type</typeparam>
+        /// <typeparam name="TResult">Result type</typeparam>
+        /// <param name="this">Source</param>
+        /// <param name="func">Functor function</param>
+        /// <returns>Result</returns>
         public static TResult Map<TSource, TResult>(this TSource @this, Func<TSource, TResult> func) =>
             func(@this);
     }
