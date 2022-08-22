079012666ad8de5788a64ea1e20dcd8b1748e76a
Ivan Rubilnik <rubilnik4@yandex.ru>
2022-08-22 14:53:58 +0500

    Adding status to RestErrorType
    Adding status to RestErrorType

--- ResultFunctional/Models/Enums/RestErrorType.cs
+++ ResultFunctional/Models/Enums/RestErrorType.cs
@@ -1,20 +1,23 @@
-﻿namespace ResultFunctional.Models.Enums
+﻿using System.Net;
+
+namespace ResultFunctional.Models.Enums
 {
     /// <summary>
     /// Rest api error types
     /// </summary>
     public enum RestErrorType
     {
-        ServerNotFound,
-        BadGateway,
-        BadRequest,
-        GatewayTimeout,
-        InternalServerError,
-        RequestTimeout,
-        RequestEntityToLarge,
-        ValueNotFound,
-        Unauthorized,
-        UnsupportedMediaType,
-        UnknownRestStatus,
+        ServerNotFound = 1,
+        BadGateway = HttpStatusCode.BadGateway,
+        BadRequest = HttpStatusCode.BadRequest,
+        Forbidden = HttpStatusCode.Forbidden,
+        GatewayTimeout = HttpStatusCode.GatewayTimeout,
+        InternalServerError = HttpStatusCode.InternalServerError,
+        RequestTimeout = HttpStatusCode.RequestTimeout,
+        RequestEntityToLarge = HttpStatusCode.RequestEntityTooLarge,
+        ValueNotFound = HttpStatusCode.NotFound,
+        Unauthorized = HttpStatusCode.Unauthorized,
+        UnsupportedMediaType = HttpStatusCode.UnsupportedMediaType,
+        UnknownRestStatus = -1,
     }
 }
