02ccc709785ac52325db75f3c9edc6a5be59aa7a
Ivan Rubi <rubilnik4@yandex.ru>
2023-04-09 13:14:05 +0500

    RValue library
    RValue library

--- ResultFunctional/FunctionalExtensions/Async/RExtension/ResultValues/ResultValueCurryCollectionAsyncExtensions.cs
+++ ResultFunctional/FunctionalExtensions/Async/RExtension/ResultValues/ResultValueCurryCollectionAsyncExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using ResultFunctional.FunctionalExtensions.Sync.RExtension.Values;
+using ResultFunctional.Models.Lists;
+using ResultFunctional.Models.Values;
 
 namespace ResultFunctional.FunctionalExtensions.Async.RExtension.ResultValues
 {
@@ -17,8 +20,10 @@ namespace ResultFunctional.FunctionalExtensions.Async.RExtension.ResultValues
         /// <param name="this">Result value one argument high order function</param>
         /// <param name="arg1">Result collection argument</param>
         /// <returns>Result value higher order function</returns>
-        public static async Task<IResultValue<Func<TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TOut>(this IResultValue<Func<IEnumerable<TIn1>, TOut>> @this,
-                                                                                                        Task<IResultCollection<TIn1>> arg1) =>
+        public static async Task<IRValue<Func<TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TOut>(this IRValue<Func<IEnumerable<TIn1>, TOut>> @this,
+                                                                                                    Task<IRList<TIn1>> arg1)
+            where TIn1 : notnull
+            where TOut : notnull =>
             await @this.
             ResultValueOk(func => (Func<IReadOnlyCollection<TIn1>, TOut>)func).
             ResultValueCurryCollectionOkAsync(arg1);
@@ -31,10 +36,11 @@ namespace ResultFunctional.FunctionalExtensions.Async.RExtension.ResultValues
         /// <param name="this">Result value one argument high order function</param>
         /// <param name="arg1">Result collection argument</param>
         /// <returns>Result value higher order function</returns>
-        public static async Task<IResultValue<Func<TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TOut>(this IResultValue<Func<IReadOnlyCollection<TIn1>, TOut>> @this,
-                                                                                        Task<IResultCollection<TIn1>> arg1) =>
-            await arg1.
-            MapTaskAsync(@this.ResultValueCurryCollectionOk);
+        public static async Task<IRValue<Func<TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TOut>(this IRValue<Func<IReadOnlyCollection<TIn1>, TOut>> @this,
+                                                                                        Task<IRList<TIn1>> arg1) =>
+            await @this.
+                ResultValueOk(func => func).
+                ResultValueCurryCollectionOkAsync(arg1);
 
         /// <summary>
         /// Get one argument result value async higher order collection function
@@ -45,8 +51,8 @@ namespace ResultFunctional.FunctionalExtensions.Async.RExtension.ResultValues
         /// <param name="this">Result value two arguments high order function</param>
         /// <param name="arg1">Result collection argument</param>
         /// <returns>Result value higher order function</returns>
-        public static async Task<IResultValue<Func<TIn2, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TOut>(this IResultValue<Func<IEnumerable<TIn1>, TIn2, TOut>> @this,
-                                                                                                    Task<IResultCollection<TIn1>> arg1) =>
+        public static async Task<IRValue<Func<TIn2, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TOut>(this IRValue<Func<IEnumerable<TIn1>, TIn2, TOut>> @this,
+                                                                                                    Task<IRList<TIn1>> arg1) =>
             await @this.
             ResultValueOk(func => (Func<IReadOnlyCollection<TIn1>, TIn2, TOut>)func).
             ResultValueCurryCollectionOkAsync(arg1);
@@ -61,8 +67,8 @@ namespace ResultFunctional.FunctionalExtensions.Async.RExtension.ResultValues
         /// <param name="this">Result value two arguments high order function</param>
         /// <param name="arg1">Result collection argument</param>
         /// <returns>Result value higher order function</returns>
-        public static async Task<IResultValue<Func<TIn2, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TOut>(this IResultValue<Func<IReadOnlyCollection<TIn1>, TIn2, TOut>> @this,
-                                                                                          Task<IResultCollection<TIn1>> arg1) =>
+        public static async Task<IRValue<Func<TIn2, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TOut>(this IRValue<Func<IReadOnlyCollection<TIn1>, TIn2, TOut>> @this,
+                                                                                          Task<IRList<TIn1>> arg1) =>
             await arg1.
             MapTaskAsync(@this.ResultValueCurryCollectionOk);
 
@@ -76,8 +82,8 @@ namespace ResultFunctional.FunctionalExtensions.Async.RExtension.ResultValues
         /// <param name="this">Result value three arguments high order function</param>
         /// <param name="arg1">Result collection argument</param>
         /// <returns>Result value higher order function</returns>
-        public static async Task<IResultValue<Func<TIn2, TIn3, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TIn3, TOut>(this IResultValue<Func<IEnumerable<TIn1>, TIn2, TIn3, TOut>> @this,
-                                                                                                    Task<IResultCollection<TIn1>> arg1) =>
+        public static async Task<IRValue<Func<TIn2, TIn3, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TIn3, TOut>(this IRValue<Func<IEnumerable<TIn1>, TIn2, TIn3, TOut>> @this,
+                                                                                                    Task<IRList<TIn1>> arg1) =>
             await @this.
             ResultValueOk(func => (Func<IReadOnlyCollection<TIn1>, TIn2, TIn3, TOut>)func).
             ResultValueCurryCollectionOkAsync(arg1);
@@ -92,8 +98,8 @@ namespace ResultFunctional.FunctionalExtensions.Async.RExtension.ResultValues
         /// <param name="this">Result value three arguments high order function</param>
         /// <param name="arg1">Result collection argument</param>
         /// <returns>Result value higher order function</returns>
-        public static async Task<IResultValue<Func<TIn2, TIn3, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TIn3, TOut>(this IResultValue<Func<IReadOnlyCollection<TIn1>, TIn2, TIn3, TOut>> @this,
-                                                                                                    Task<IResultCollection<TIn1>> arg1) =>
+        public static async Task<IRValue<Func<TIn2, TIn3, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TIn3, TOut>(this IRValue<Func<IReadOnlyCollection<TIn1>, TIn2, TIn3, TOut>> @this,
+                                                                                                    Task<IRList<TIn1>> arg1) =>
             await arg1.
             MapTaskAsync(@this.ResultValueCurryCollectionOk);
 
@@ -108,8 +114,8 @@ namespace ResultFunctional.FunctionalExtensions.Async.RExtension.ResultValues
         /// <param name="this">Result value four arguments high order function</param>
         /// <param name="arg1">Result collection argument</param>
         /// <returns>Result value higher order function</returns>
-        public static async Task<IResultValue<Func<TIn2, TIn3, TIn4, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TIn3, TIn4, TOut>(this IResultValue<Func<IEnumerable<TIn1>, TIn2, TIn3, TIn4, TOut>> @this,
-                                                                                                                            Task<IResultCollection<TIn1>> arg1) =>
+        public static async Task<IRValue<Func<TIn2, TIn3, TIn4, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TIn3, TIn4, TOut>(this IRValue<Func<IEnumerable<TIn1>, TIn2, TIn3, TIn4, TOut>> @this,
+                                                                                                                            Task<IRList<TIn1>> arg1) =>
             await @this.
             ResultValueOk(func => (Func<IReadOnlyCollection<TIn1>, TIn2, TIn3, TIn4, TOut>)func).
             ResultValueCurryCollectionOkAsync(arg1);
@@ -125,8 +131,8 @@ namespace ResultFunctional.FunctionalExtensions.Async.RExtension.ResultValues
         /// <param name="this">Result value four arguments high order function</param>
         /// <param name="arg1">Result collection argument</param>
         /// <returns>Result value higher order function</returns>
-        public static async Task<IResultValue<Func<TIn2, TIn3, TIn4, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TIn3, TIn4, TOut>(this IResultValue<Func<IReadOnlyCollection<TIn1>, TIn2, TIn3, TIn4, TOut>> @this,
-                                                                                                                  Task<IResultCollection<TIn1>> arg1) =>
+        public static async Task<IRValue<Func<TIn2, TIn3, TIn4, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TIn3, TIn4, TOut>(this IRValue<Func<IReadOnlyCollection<TIn1>, TIn2, TIn3, TIn4, TOut>> @this,
+                                                                                                                  Task<IRList<TIn1>> arg1) =>
              await arg1.
             MapTaskAsync(@this.ResultValueCurryCollectionOk);
 
@@ -142,8 +148,8 @@ namespace ResultFunctional.FunctionalExtensions.Async.RExtension.ResultValues
         /// <param name="this">Result value five arguments high order function</param>
         /// <param name="arg1">Result collection argument</param>
         /// <returns>Result value higher order function</returns>
-        public static async Task<IResultValue<Func<TIn2, TIn3, TIn4, TIn5, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TIn3, TIn4, TIn5, TOut>(this IResultValue<Func<IEnumerable<TIn1>, TIn2, TIn3, TIn4, TIn5, TOut>> @this,
-                                                                                                                                        Task<IResultCollection<TIn1>> arg1) =>
+        public static async Task<IRValue<Func<TIn2, TIn3, TIn4, TIn5, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TIn3, TIn4, TIn5, TOut>(this IRValue<Func<IEnumerable<TIn1>, TIn2, TIn3, TIn4, TIn5, TOut>> @this,
+                                                                                                                                        Task<IRList<TIn1>> arg1) =>
             await @this.
             ResultValueOk(func => (Func<IReadOnlyCollection<TIn1>, TIn2, TIn3, TIn4, TIn5, TOut>)func).
             ResultValueCurryCollectionOkAsync(arg1);
@@ -160,8 +166,8 @@ namespace ResultFunctional.FunctionalExtensions.Async.RExtension.ResultValues
         /// <param name="this">Result value five arguments high order function</param>
         /// <param name="arg1">Result collection argument</param>
         /// <returns>Result value higher order function</returns>
-        public static async Task<IResultValue<Func<TIn2, TIn3, TIn4, TIn5, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TIn3, TIn4, TIn5, TOut>(this IResultValue<Func<IReadOnlyCollection<TIn1>, TIn2, TIn3, TIn4, TIn5, TOut>> @this,
-                                                                                                                             Task<IResultCollection<TIn1>> arg1) =>
+        public static async Task<IRValue<Func<TIn2, TIn3, TIn4, TIn5, TOut>>> ResultValueCurryCollectionOkAsync<TIn1, TIn2, TIn3, TIn4, TIn5, TOut>(this IRValue<Func<IReadOnlyCollection<TIn1>, TIn2, TIn3, TIn4, TIn5, TOut>> @this,
+                                                                                                                             Task<IRList<TIn1>> arg1) =>
             await arg1.
             MapTaskAsync(@this.ResultValueCurryCollectionOk);
     }
